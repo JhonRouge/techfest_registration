72f12f6021b658101674c64fd7174359807439b6
Andales <12411812@benilde.edu.ph>
2025-11-06 12:38:47 +0800

    Duplicate Name Detection

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,12 +1,13 @@
 print("Welcome to SMIT TechFest!")
 print("Event organized by Jhon Rouge Andales of APPDAET BTCS2")
-
+#T1
 n_par = int(input("How many participants will register?: "))
 
 if n_par <= 0:
     print("Invalid number of participants.")
     quit()
 
+#T2
 par = []
 for i in range(n_par):
     name = input("Enter participant name: ")
@@ -17,8 +18,23 @@ print("registered participants: ")
 for i, p in enumerate(par,1):
     print(f"{i}. {p['name']} - {p['track']}")
 
+#T3
 tracks = {p["track"] for p in par}
 print("\nTracks to offer in this year: ")
 print(", ".join(tracks))
 if len(tracks) < 2:
     print("No more tracks to offer.")
+
+#T4
+names = set()
+dup = set()
+for p in par:
+    if p["name"] in names:
+        dup.add(p["name"])
+    names.add(p["name"])
+
+if dup:
+    for i in dup:
+        print(f"Duplicated name: {i}")
+else:
+    print("No duplicated names.")
