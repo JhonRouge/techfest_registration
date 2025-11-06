a35806315a35b7dcd34a12bab5c71dee9ef84c5b
Andales <12411812@benilde.edu.ph>
2025-11-06 12:11:25 +0800

    Collect Participant Information

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,11 +1,18 @@
 print("Welcome to SMIT TechFest!")
 print("Event organized by Jhon Rouge Andales of APPDAET BTCS2")
 
-par = input("How many participants will register?: ")
-while True:
-    if par == "1":
+n_par = int(input("How many participants will register?: "))
 
+if n_par <= 0:
+    print("Invalid number of participants.")
+    quit()
 
-    else:
-        print("Invalid number of participants")
-        break
+par = []
+for i in range(n_par):
+    name = input("Enter participant name: ")
+    track = input("Enter a track: ")
+    par.append({"name": name,"track": track})
+
+print("registered participants: ")
+for i, p in enumerate(par,1):
+    print(f"{i}. {p['name']} - {p['track']}")
