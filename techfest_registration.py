5abc6234fccf43f61536eca1ce971b403b069bd1
Andales <12411812@benilde.edu.ph>
2025-11-06 11:52:42 +0800

    Registration Setup

---
+++ techfest_registration.py
@@ -0,0 +1,11 @@
+print("Welcome to SMIT TechFest!")
+print("Event organized by Jhon Rouge Andales of APPDAET BTCS2")
+
+par = input("How many participants will register?: ")
+while True:
+    if par == "1":
+
+
+    else:
+        print("Invalid number of participants")
+        break
