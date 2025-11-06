44fe14ece4d2c2e4f9cd4a8537466ed19bda19c7
Andales <12411812@benilde.edu.ph>
2025-11-06 12:15:02 +0800

    Registration Setup

--- techfest_registration.py
+++ techfest_registration.py
@@ -7,12 +7,3 @@ if n_par <= 0:
     print("Invalid number of participants.")
     quit()
 
-par = []
-for i in range(n_par):
-    name = input("Enter participant name: ")
-    track = input("Enter a track: ")
-    par.append({"name": name,"track": track})
-
-print("registered participants: ")
-for i, p in enumerate(par,1):
-    print(f"{i}. {p['name']} - {p['track']}")
